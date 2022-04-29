cbe8003963d044825783cd385accd2c35ce616ae
Jan Krupiński <jk.krupinski@gmail.com>
2022-04-29 19:59:17 +0200

    Build: Build project

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,42 @@
 import resolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+import commonJS from 'rollup-plugin-commonjs';
+import dts from 'rollup-plugin-dts';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 
+const packageJSON = require('./package.json');
+
 export default [
   {
-    input: './src/index.ts',
+    input: 'src/index.ts',
     output: [
       {
-        file: 'dist/index.js',
+        file: packageJSON.main,
         format: 'cjs',
+        sourcemap: true,
+        name: 'ReactPdfPrinter',
       },
       {
-        file: 'dist/index.es.js',
-        format: 'es',
-        exports: 'named',
+        file: packageJSON.module,
+        format: 'esm',
+        sourcemap: true,
       },
     ],
     plugins: [
-      postcss({
-        plugins: [],
-        minimize: true,
-      }),
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-react'],
-      }),
       external(),
       resolve(),
+      commonJS(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss(),
+      terser(),
     ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    external: [/\/css$/],
+    plugins: [dts()],
   },
 ];
