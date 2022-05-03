35272f1af696ea9303dd6099f7e4f83253883c57
Jan Krupiński <jk.krupinski@gmail.com>
2022-05-03 21:35:26 +0200

    Size: Import css

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,12 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonJS from 'rollup-plugin-commonjs';
+import copy from 'rollup-plugin-copy';
 import dts from 'rollup-plugin-dts';
+import css from 'rollup-plugin-import-css';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+import styles from 'rollup-plugin-styles';
 import { terser } from 'rollup-plugin-terser';
 
 const packageJSON = require('./package.json');
@@ -34,9 +37,14 @@ export default [
     ],
   },
   {
+    input: 'src/style.css',
+    output: [{ dir: 'dist/esm/types' }],
+    plugins: [postcss({ extract: 'style.css' })],
+  },
+  {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    external: [/\/css$/],
-    plugins: [dts()],
+    external: [/\/.css$/u],
+    plugins: [dts(), postcss()],
   },
 ];
