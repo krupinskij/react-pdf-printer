ba8742ca05e1107693bcf1b65b8353adfb37d293
Jan Krupiński <jk.krupinski@gmail.com>
2022-10-12 19:34:01 +0200

    Loading: Allow passing function to screen view

--- src/components/Document/Document.tsx
+++ src/components/Document/Document.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, ReactElement } from 'react';
+import React, { ReactNode, ReactElement, useState, useCallback } from 'react';
 
 import DocumentProvider from 'context/PrinterProvider';
 
@@ -11,12 +11,13 @@ type DocumentProps = {
   header: ReactNode;
   footer: ReactNode;
   children: ReactElement<ArticleProps> | Array<ReactElement<ArticleProps>>;
-  screen?: ReactNode;
+  screen?: ReactNode | ((isLoading: boolean) => ReactNode);
   configuration?: DocumentConfiguration;
   onLoaded?: () => void;
 };
 
 const Document = ({ header, footer, children, screen, configuration, onLoaded }: DocumentProps) => {
+  const [isLoading, setIsLoading] = useState(true);
   const { isAsync = false } = configuration || {};
   const documentChildren = React.Children.map(children, (child) => {
     if (
@@ -33,12 +34,18 @@ const Document = ({ header, footer, children, screen, configuration, onLoaded }:
     return React.cloneElement(child, props);
   });
 
+  const handleLoaded = useCallback(() => {
+    setIsLoading(false);
+    onLoaded?.();
+  }, [onLoaded, setIsLoading]);
+
+  const screenChild = typeof screen === 'function' ? screen(isLoading) : screen;
   return (
     <DocumentProvider isAsync={isAsync}>
-      <Content configuration={configuration} printOnly={!!screen} onLoaded={onLoaded}>
+      <Content configuration={configuration} printOnly={!!screen} onLoaded={handleLoaded}>
         {documentChildren}
       </Content>
-      <div data-printer-screenonly="true">{screen}</div>
+      <div data-printer-screenonly="true">{screenChild}</div>
     </DocumentProvider>
   );
 };
